938f4f56f4107ebae46768c8f98647b59213f08a
sam <247765564@qq.com>
2020-03-28 14:17:59 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <sales-report />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import SalesReport from './components/SalesReport'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      SalesReport
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html, body {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+
+  #app {
+    width: 100%;
+    height: 100%;
+  }
 </style>
---
+++ src/components/SalesReport/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="sales-report">电商销售趋势组件</div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .sales-report {
+  }
+</style>
